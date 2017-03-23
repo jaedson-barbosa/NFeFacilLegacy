ed40412990603de6c874d5591398c16f17abcd0b
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-03-23 15:18:13 -0300

    Adicionadas classes de validação e de Log.

---
+++ NFeFacil/Log/Toast.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Notifications;
+
+namespace NFeFacil.Log
+{
+    public sealed class Toast : ILog
+    {
+        public void Escrever(TitulosComuns título, string texto)
+        {
+            var toastNotifier = ToastNotificationManager.CreateToastNotifier();
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
+            var textNodes = toastXml.GetElementsByTagName("text");
+            textNodes[0].AppendChild(toastXml.CreateTextNode(título.ToString()));
+            textNodes[1].AppendChild(toastXml.CreateTextNode(texto));
+            toastNotifier.Show(new ToastNotification(toastXml));
+        }
+    }
+}

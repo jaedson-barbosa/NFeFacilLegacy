a2cf3d831413fb23946945ca8229a4d714eadb7f
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-03-23 15:26:46 -0300

    Classes de configurações já foram obtidas.

---
+++ NFeFacil/Configuracoes/ConfiguracoesCertificacao.cs
@@ -0,0 +1,13 @@
+﻿using static NFeFacil.Configuracoes.ConfiguracoesLocais;
+
+namespace NFeFacil.Configuracoes
+{
+    public static class ConfiguracoesCertificacao
+    {
+        public static string Certificado
+        {
+            get { return Pasta.Values[nameof(Certificado)] as string; }
+            set { Pasta.Values[nameof(Certificado)] = value; }
+        }
+    }
+}
---
+++ NFeFacil/Configuracoes/ConfiguracoesLocais.cs
@@ -0,0 +1,9 @@
+﻿using Windows.Storage;
+
+namespace NFeFacil.Configuracoes
+{
+    internal static class ConfiguracoesLocais
+    {
+        internal static readonly ApplicationDataContainer Pasta = ApplicationData.Current.LocalSettings;
+    }
+}

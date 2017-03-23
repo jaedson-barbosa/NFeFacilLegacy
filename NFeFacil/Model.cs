1144ee50bde6b116594131d76d4ac11db9753782
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-03-23 13:36:20 -0300

    Adicionar arquivos de projeto.

---
+++ NFeFacil/Model.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace NFeFacil
+{
+    public class DadosBaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=dadosBase.db");
+        }
+    }
+}

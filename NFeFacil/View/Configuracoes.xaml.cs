735ae3bcb1e1e7e256c387d243dfe05f3b6afe15
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-03-28 16:01:10 -0300

    Algumas pequenas simplificações e renomeações.

--- NFeFacil/View/Configuracoes.xaml.cs
+++ NFeFacil/View/Configuracoes.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
@@ -21,27 +20,6 @@ namespace NFeFacil.View
         {
             ocultarGrid.Begin();
             await Task.Delay(250);
-        }
-    }
-
-    public sealed class ExibicaoQR : StateTriggerBase
-    {
-        public bool Visivel { get; set; }
-
-        private ViewModel.Configuracoes.Sincronizacao contexto;
-        public ViewModel.Configuracoes.Sincronizacao Contexto
-        {
-            get => contexto;
-            set
-            {
-                contexto = value;
-                contexto.MostrarQRChanged += Contexto_MostrarQRChanged;
-            }
-        }
-
-        private void Contexto_MostrarQRChanged(ViewModel.Configuracoes.Sincronizacao sender, ViewModel.Configuracoes.Sincronizacao.MostrarQRChangeEventArgs args)
-        {
-            SetActive(Visivel == args.DadoAtual);
         }
     }
 }
---
+++ NFeFacil/View/ExibicaoQR.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace NFeFacil.View
+{
+    public sealed class ExibicaoQR : StateTriggerBase
+    {
+        public bool Visivel { get; set; }
+
+        private ViewModel.Configuracoes.Sincronizacao contexto;
+        public ViewModel.Configuracoes.Sincronizacao Contexto
+        {
+            get => contexto;
+            set
+            {
+                contexto = value;
+                contexto.MostrarQRChanged += Contexto_MostrarQRChanged;
+            }
+        }
+
+        private void Contexto_MostrarQRChanged(ViewModel.Configuracoes.Sincronizacao sender, ViewModel.Configuracoes.Sincronizacao.MostrarQRChangeEventArgs args)
+        {
+            SetActive(Visivel == args.DadoAtual);
+        }
+    }
+}

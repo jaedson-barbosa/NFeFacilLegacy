9c7e95839257d517eca4805e9a19267d68e8c87b
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-04-24 14:21:56 -0300

    Removidos usos desnecessários.

--- Background/ClienteBackground.cs
+++ Background/ClienteBackground.cs
@@ -1,10 +1,5 @@
 ﻿using BibliotecaCentral.Log;
 using BibliotecaCentral.Sincronizacao;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 
 namespace Background

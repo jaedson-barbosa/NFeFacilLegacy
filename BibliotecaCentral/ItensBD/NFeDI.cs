22da551d177dfaeff373de6e4fd2b45214859d43
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-04-25 13:03:17 -0300

    Removidos conversores explícidos para NFeDI.

--- BibliotecaCentral/ItensBD/NFeDI.cs
+++ BibliotecaCentral/ItensBD/NFeDI.cs
@@ -20,36 +20,34 @@ namespace BibliotecaCentral.ItensBD
         [Required]
         public int Status { get; set; }
 
-        public static explicit operator NFeDI(NFe nota)
-        {
-            return new NFeDI
-            {
-                Id = nota.Informações.Id,
-                NomeCliente = nota.Informações.destinatário.nome,
-                NomeEmitente = nota.Informações.emitente.nome,
-                DataEmissao = nota.Informações.identificação.DataHoraEmissão,
-                NumeroNota = nota.Informações.identificação.Numero.ToString(),
-                Status = nota.Signature!= null && nota.Signature.HasElements ? (int)StatusNFe.Assinado : (int)StatusNFe.Salvo
-            };
-        }
-
-        public static explicit operator NFeDI(Processo nota)
-        {
-            return new NFeDI
-            {
-                Id = nota.NFe.Informações.Id,
-                NomeCliente = nota.NFe.Informações.destinatário.nome,
-                NomeEmitente = nota.NFe.Informações.emitente.nome,
-                DataEmissao = nota.NFe.Informações.identificação.DataHoraEmissão,
-                NumeroNota = nota.NFe.Informações.identificação.Numero.ToString(),
-                Status = nota.ProtNFe != null ? (int)StatusNFe.Emitido : nota.NFe.Signature != null && nota.NFe.Signature.HasElements ? (int)StatusNFe.Assinado : (int)StatusNFe.Salvo
-            };
-        }
-
         public static NFeDI Converter(XElement xml)
         {
-            if (xml.Name.LocalName == nameof(NFe)) return (NFeDI)xml.FromXElement<NFe>();
-            else return (NFeDI)xml.FromXElement<Processo>();
+            if (xml.Name.LocalName == nameof(NFe))
+            {
+                var nota = xml.FromXElement<NFe>();
+                return new NFeDI
+                {
+                    Id = nota.Informações.Id,
+                    NomeCliente = nota.Informações.destinatário.nome,
+                    NomeEmitente = nota.Informações.emitente.nome,
+                    DataEmissao = nota.Informações.identificação.DataHoraEmissão,
+                    NumeroNota = nota.Informações.identificação.Numero.ToString(),
+                    Status = nota.Signature != null && nota.Signature.HasElements ? (int)StatusNFe.Assinado : (int)StatusNFe.Salvo
+                };
+            }
+            else
+            {
+                var nota = xml.FromXElement<Processo>();
+                return new NFeDI
+                {
+                    Id = nota.NFe.Informações.Id,
+                    NomeCliente = nota.NFe.Informações.destinatário.nome,
+                    NomeEmitente = nota.NFe.Informações.emitente.nome,
+                    DataEmissao = nota.NFe.Informações.identificação.DataHoraEmissão,
+                    NumeroNota = nota.NFe.Informações.identificação.Numero.ToString(),
+                    Status = nota.ProtNFe != null ? (int)StatusNFe.Emitido : nota.NFe.Signature != null && nota.NFe.Signature.HasElements ? (int)StatusNFe.Assinado : (int)StatusNFe.Salvo
+                };
+            }
         }
 
         public async Task<object> ConjuntoCompletoAsync()

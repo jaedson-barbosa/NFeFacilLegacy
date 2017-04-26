b2a5d27eadd4269b346f7249184ea059df989e40
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-04-26 09:19:56 -0300

    Teste de nova forma de iniciar a sincronização no background,

--- Background/ClienteBackground.cs
+++ Background/ClienteBackground.cs
@@ -1,19 +1,22 @@
 ﻿using BibliotecaCentral.Log;
 using BibliotecaCentral.Sincronizacao;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 
 namespace Background
 {
     public sealed class ClienteBackground : IBackgroundTask
     {
-        public async void Run(IBackgroundTaskInstance taskInstance)
+        public void Run(IBackgroundTaskInstance taskInstance)
         {
             var deferral = taskInstance.GetDeferral();
             var toast = new Toast();
             try
             {
                 toast.Escrever(TitulosComuns.Iniciando, "Iniciando sincronização em background.");
-                await new GerenciadorCliente(toast).Sincronizar(DadosSincronizaveis.Tudo, true);
+                var task = Task.Run(() => new GerenciadorCliente(toast).Sincronizar(DadosSincronizaveis.Tudo, true));
+                task.Wait();
+                toast.Escrever(TitulosComuns.Finalizando, "Sincronizacao concluida");
             }
             catch (System.Exception e)
             {

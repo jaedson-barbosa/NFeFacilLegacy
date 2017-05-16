9764177178908f3631d0a25968e88b473795d48c
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-05-16 16:47:20 -0300

    Melhorado o desempenho do painél adaptável.

--- NFeFacil/View/Estilos/Auxiliares/PainelAdaptavel.cs
+++ NFeFacil/View/Estilos/Auxiliares/PainelAdaptavel.cs
@@ -10,41 +10,47 @@ namespace NFeFacil.View.Estilos.Auxiliares
         private double _maxWidth;
         private double _maxHeight;
         private double colunas;
-        private double linhas;
 
         public double TamanhoDesejado { get; set; }
 
         protected override Size MeasureOverride(Size availableSize)
         {
+            var tamanhoComparacao = TamanhoDesejado == 0 ? availableSize : new Size(TamanhoDesejado, availableSize.Height);
             _maxWidth = Children.Max(x =>
             {
-                x.Measure(TamanhoDesejado == 0 ? availableSize : new Size(TamanhoDesejado, availableSize.Height));
+                x.Measure(tamanhoComparacao);
                 return x.DesiredSize.Width;
             });
             _maxHeight = Children.Max(x => x.DesiredSize.Height);
 
             colunas = Math.Floor(availableSize.Width / _maxWidth);
-            if (colunas > 5) colunas = 5;
             _maxWidth = Math.Floor(availableSize.Width / colunas);
-            linhas = Math.Ceiling(Children.Count / colunas);
-            return new Size(_maxWidth * colunas, _maxHeight * linhas);
+            return new Size(_maxWidth * colunas, _maxHeight * Math.Ceiling(Children.Count / colunas));
         }
 
         protected override Size ArrangeOverride(Size finalSize)
         {
-            var espacosX = from x in Enumerable.Range(0, (int)colunas)
-                           select x * _maxWidth;
-            var espacosY = from y in Enumerable.Range(0, (int)linhas)
-                           select y * _maxHeight;
             var tamanho = new Size(_maxWidth, _maxHeight);
-            for (int linha = 0, i = 0; linha < linhas; linha++)
+            double maxX = (colunas - 1) * _maxWidth;
+
+            double x = 0;
+            double y = 0;
+            for (int k = 0; k < Children.Count; k++)
             {
-                for (int coluna = 0; coluna < colunas && i < Children.Count; coluna++, i++)
+                var inicio = new Point(x, y);
+                Children[k].Arrange(new Rect(inicio, tamanho));
+
+                if (x >= maxX)
                 {
-                    var inicio = new Point(espacosX.ElementAt(coluna), espacosY.ElementAt(linha));
-                    Children[i].Arrange(new Rect(inicio, tamanho));
+                    x = 0;
+                    y += _maxHeight;
+                }
+                else
+                {
+                    x += _maxWidth;
                 }
             }
+
             return finalSize;
         }
     }

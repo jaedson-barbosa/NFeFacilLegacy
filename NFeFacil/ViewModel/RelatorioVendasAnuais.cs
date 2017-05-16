765c69385afd76c0bd51626d8a488dda128374b8
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-05-16 15:22:08 -0300

    Removido travamento ao ver o relatório de vendas anual.

--- NFeFacil/ViewModel/RelatorioVendasAnuais.cs
+++ NFeFacil/ViewModel/RelatorioVendasAnuais.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace NFeFacil.ViewModel
 {
@@ -14,22 +15,32 @@ namespace NFeFacil.ViewModel
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public ObservableCollection<int> AnosDisponiveis
+        public ObservableCollection<int> AnosDisponiveis { get; private set; }
+        public ObservableCollection<TotalPorMes> ResultadoMes { get; private set; }
+        public ObservableCollection<TotalPorCliente> ResultadoCliente { get; private set; }
+
+        public RelatorioVendasAnuais()
         {
-            get
+            DefinirTudo();
+
+            async void DefinirTudo()
             {
-                using (var db = new NotasFiscais())
-                {
-                    var anos = from dado in db.Registro
-                               let ano = Convert.ToDateTime(dado.DataEmissao).Year
-                               orderby ano ascending
-                               select ano;
-                    return anos.Distinct().GerarObs();
-                }
+                await DefinirAnosDisponiveis();
             }
         }
 
-        private List<NFe> notas;
+        private async Task DefinirAnosDisponiveis()
+        {
+            using (var db = new NotasFiscais())
+            {
+                AnosDisponiveis = await Task.Run(() => (from dado in db.Registro
+                                                        let ano = Convert.ToDateTime(dado.DataEmissao).Year
+                                                        orderby ano ascending
+                                                        select ano).Distinct().GerarObs());
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnosDisponiveis)));
+            }
+        }
+
 
         private int anoEscolhido;
         public int AnoEscolhido
@@ -44,98 +55,65 @@ namespace NFeFacil.ViewModel
 
         private async void AnoMudou()
         {
+            IEnumerable<NFe> notas;
             using (var db = new NotasFiscais())
             {
-                notas = new List<NFe>();
-                foreach (var item in await db.RegistroAsync())
-                {
-                    if (item.nota.Status < 4)
-                    {
-                        notas.Add(item.xml.FromXElement<NFe>());
-                    }
-                    else
-                    {
-                        var proc = item.xml.FromXElement<Processo>();
-                        notas.Add(proc.NFe);
-                    }
-                }
+                notas = from item in await db.RegistroAsync()
+                        let xml = item.xml
+                        let usarNFe = item.nota.Status < 4
+                        let nota = usarNFe ? xml.FromXElement<NFe>() : (xml.FromXElement<Processo>()).NFe
+                        select nota;
             }
+
+            var totalCliente = new List<TotalPorCliente>();
+            foreach (var item in notas)
+            {
+                var det = item.Informações;
+                if (totalCliente.Count(x => x.Doc == det.destinatário.Documento) == 0)
+                    totalCliente.Add(new TotalPorCliente { Doc = det.destinatário.Documento });
+                var tot = totalCliente.Single(x => x.Doc == det.destinatário.Documento);
+                tot.Mun = det.destinatário.endereco.NomeMunicipio;
+                tot.Nome = det.destinatário.nome;
+                foreach (var prod in det.produtos)
+                {
+                    tot.Quantidade += prod.Produto.QuantidadeComercializada;
+                }
+                tot.Total += det.total.ICMSTot.vNF;
+            }
+            totalCliente.Add(new TotalPorCliente
+            {
+                Nome = "Total",
+                Quantidade = totalCliente.Sum(x => x.Quantidade),
+                Total = totalCliente.Sum(x => x.Total)
+            });
+            ResultadoCliente = (from item in totalCliente
+                                 orderby item.Total descending
+                                 select item).GerarObs();
             PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoCliente)));
+
+            var totalMes = new List<TotalPorMes>(12);
+            for (int i = 1; i < 13; i++)
+            {
+                totalMes.Add(new TotalPorMes { Mês = i.ToString() });
+            }
+            foreach (var item in notas)
+            {
+                var det = item.Informações;
+                var data = Convert.ToDateTime(det.identificação.DataHoraEmissão);
+                foreach (var prod in det.produtos)
+                {
+                    totalCliente[data.Month - 1].Quantidade += prod.Produto.QuantidadeComercializada;
+                }
+                totalCliente[data.Month - 1].Total += det.total.ICMSTot.vNF;
+            }
+            totalMes.Add(new TotalPorMes
+            {
+                Mês = "Total",
+                Quantidade = totalCliente.Sum(x => x.Quantidade),
+                Total = totalCliente.Sum(x => x.Total)
+            });
+            ResultadoMes = totalMes.GerarObs();
             PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoMes)));
-        }
-
-        public ObservableCollection<TotalPorMes> ResultadoMes
-        {
-            get
-            {
-                if (notas != null)
-                {
-                    var total = new List<TotalPorMes>(12);
-                    for (int i = 1; i < 13; i++)
-                    {
-                        total.Add(new TotalPorMes { Mês = i.ToString() });
-                    }
-                    foreach (var item in notas)
-                    {
-                        var det = item.Informações;
-                        var data = Convert.ToDateTime(det.identificação.DataHoraEmissão);
-                        foreach (var prod in det.produtos)
-                        {
-                            total[data.Month - 1].Quantidade += prod.Produto.QuantidadeComercializada;
-                        }
-                        total[data.Month - 1].Total += det.total.ICMSTot.vNF;
-                    }
-                    total.Add(new TotalPorMes
-                    {
-                        Mês = "Total",
-                        Quantidade = total.Sum(x => x.Quantidade),
-                        Total = total.Sum(x => x.Total)
-                    });
-                    return total.GerarObs();
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
-
-        public ObservableCollection<TotalPorCliente> ResultadoCliente
-        {
-            get
-            {
-                if (notas != null)
-                {
-                    var total = new List<TotalPorCliente>();
-                    foreach (var item in notas)
-                    {
-                        var det = item.Informações;
-                        if (total.Count(x => x.Doc == det.destinatário.Documento) == 0)
-                            total.Add(new TotalPorCliente { Doc = det.destinatário.Documento });
-                        var tot = total.Single(x => x.Doc == det.destinatário.Documento);
-                        tot.Mun = det.destinatário.endereco.NomeMunicipio;
-                        tot.Nome = det.destinatário.nome;
-                        foreach (var prod in det.produtos)
-                        {
-                            tot.Quantidade += prod.Produto.QuantidadeComercializada;
-                        }
-                        tot.Total += det.total.ICMSTot.vNF;
-                    }
-                    total.Add(new TotalPorCliente
-                    {
-                        Nome = "Total",
-                        Quantidade = total.Sum(x => x.Quantidade),
-                        Total = total.Sum(x => x.Total)
-                    });
-                    return (from item in total
-                            orderby item.Total descending
-                            select item).GerarObs();
-                }
-                else
-                {
-                    return null;
-                }
-            }
         }
 
         public sealed class TotalPorCliente

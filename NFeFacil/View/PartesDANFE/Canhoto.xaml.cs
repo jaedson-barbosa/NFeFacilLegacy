455009f2723f90bf173d169727c307bf82f9fdbf
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-05-26 16:20:55 -0300

    O canhoto está ponto e já conta com o picote; O controle dos dados da NFe foi criado mas ainda está em branco.

--- NFeFacil/View/PartesDANFE/Canhoto.xaml.cs
+++ NFeFacil/View/PartesDANFE/Canhoto.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Windows.Foundation;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
@@ -15,30 +14,6 @@ namespace NFeFacil.View.PartesDANFE
         public Canhoto()
         {
             InitializeComponent();
-        }
-    }
-
-    public sealed class DimensoesPadrao
-    {
-        public Thickness PaddingCampoPadrao => new Thickness(CentimeterToPixel(0.3), CentimeterToPixel(0.15), CentimeterToPixel(0.15), 0);
-        public GridLength AlturaLinhaPadrao
-        {
-            get
-            {
-                var retorno = CentimeterToLength(0.85);
-                return retorno;
-            }
-        }
-
-        static double CentimeterToPixel(double Centimeter)
-        {
-            const double fator = 96 / 2.54;
-            return Centimeter * fator;
-        }
-
-        internal static GridLength CentimeterToLength(double Centimeter)
-        {
-            return new GridLength(CentimeterToPixel(Centimeter));
         }
     }
 }
---
+++ NFeFacil/View/PartesDANFE/DimensoesPadrao.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NFeFacil.View.PartesDANFE
+{
+    public sealed class DimensoesPadrao
+    {
+        public GridLength AlturaLinhaPadrao => CentimeterToLength(0.85);
+        public Thickness MargemBloco => new Thickness(0, 0, 0, CentimeterToPixel(0.2));
+
+        static double CentimeterToPixel(double Centimeter)
+        {
+            const double fator = 96 / 2.54;
+            return Centimeter * fator;
+        }
+
+        internal static GridLength CentimeterToLength(double Centimeter)
+        {
+            return new GridLength(CentimeterToPixel(Centimeter));
+        }
+    }
+}

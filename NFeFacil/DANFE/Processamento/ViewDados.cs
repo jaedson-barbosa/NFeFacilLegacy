36b6d576e701723160d58dd49e6eaa090ebeb30b
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-05-30 17:30:47 -0300

    Criada a geração dinâmica de campos dos dados adicionais em página única do DANFE.

--- NFeFacil/DANFE/Processamento/ViewDados.cs
+++ NFeFacil/DANFE/Processamento/ViewDados.cs
@@ -7,121 +7,12 @@ using BibliotecaCentral.ModeloXML.PartesProcesso.PartesNFe.PartesDetalhes.Partes
 using BibliotecaCentral.ModeloXML.PartesProcesso.PartesNFe.PartesDetalhes.PartesTotal;
 using NFeFacil.DANFE.Pacotes;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
 
 namespace NFeFacil.DANFE.Processamento
 {
-    public sealed class ViewDados
+    internal static class ViewDados
     {
-        private ConexãoDados conec;
-        public int TotalPaginas { get; private set; }
-        private DadosPrimeiraPagina dadosPrimeiraPagina;
-        private List<DadosOutrasPaginas> dadosOutrasPaginas = new List<DadosOutrasPaginas>();
-
-        public ViewDados(ref WebView view, Processo processo)
-        {
-            conec = new ConexãoDados(ref view);
-            AdicionarFuncoesPaginacao(Converter(processo));
-        }
-
-        void AdicionarFuncoesPaginacao(Geral dadosDanfe)
-        {
-            if (dadosDanfe._DadosProdutos.Length == 0)
-                throw new ArgumentNullException(nameof(dadosDanfe._DadosProdutos));
-            
-            var nProdutosPagina1 = 18 - (dadosDanfe._Duplicatas == null ? 0 : dadosDanfe._Duplicatas.Length);
-            var nProdutosPaginaRestante = 25;
-
-            if (dadosDanfe._DadosProdutos.Length <= nProdutosPagina1)
-            {
-                TotalPaginas = 1;
-            }
-            else if (dadosDanfe._DadosProdutos.Length <= nProdutosPagina1 + nProdutosPaginaRestante)
-            {
-                TotalPaginas = 2;
-            }
-            else
-            {
-                double quantRestante = dadosDanfe._DadosProdutos.Length - (nProdutosPagina1 + nProdutosPaginaRestante);
-                var nExtraQuebrado = quantRestante / nProdutosPaginaRestante;
-                var resto = quantRestante % nProdutosPaginaRestante;
-                TotalPaginas = (int)nExtraQuebrado + 2;
-                if (resto > 0) TotalPaginas++;
-            }
-
-            var nItensPorPagina = new List<int>();
-            if (TotalPaginas == 1)
-            {
-                nItensPorPagina.Add(nProdutosPagina1);
-            }
-            else if (TotalPaginas == 2)
-            {
-                nItensPorPagina.Add(nProdutosPagina1);
-                nItensPorPagina.Add(nProdutosPaginaRestante);
-            }
-            else
-            {
-                nItensPorPagina.Add(nProdutosPagina1);
-                for (int i = 1; i < TotalPaginas; i++)
-                {
-                    nItensPorPagina.Add(nProdutosPaginaRestante);
-                }
-            }
-
-            var paginas = new Dictionary<int, List<DadosProduto>>();
-            for (int i = 0; i < TotalPaginas; i++) paginas.Add(i, new List<DadosProduto>());
-
-            var pagina = 0;
-            foreach (var Produto in dadosDanfe._DadosProdutos)
-            {
-                if (paginas[pagina].Count == nItensPorPagina[pagina]) pagina++;
-                paginas[pagina].Add(Produto);
-            }
-
-            for (int i = 0; i < paginas.Keys.Count; i++)
-            {
-                dadosDanfe._DadosNFe.DefinirPagina(TotalPaginas, i + 1);
-
-                if (i == 0)
-                {
-                    dadosPrimeiraPagina = new DadosPrimeiraPagina
-                    {
-                        cabec = dadosDanfe._DadosCabecalho,
-                        nfe = dadosDanfe._DadosNFe,
-                        cliente = dadosDanfe._DadosCliente,
-                        motorista = dadosDanfe._DadosMotorista,
-                        imposto = dadosDanfe._DadosImposto,
-                        Produto = paginas[i],
-                        extras = dadosDanfe._DadosAdicionais,
-                        paginaTotal = paginas.Keys.Count,
-                        duplicatas = dadosDanfe._Duplicatas
-                    };
-                }
-                else
-                {
-                    dadosOutrasPaginas.Add(new DadosOutrasPaginas
-                    {
-                        nfe = dadosDanfe._DadosNFe,
-                        cliente = dadosDanfe._DadosCliente,
-                        extras = dadosDanfe._DadosAdicionais,
-                        paginaAtual = i + 1,
-                        paginaTotal = paginas.Keys.Count,
-                        Produto = paginas[i]
-                    });
-                }
-            }
-        }
-
-        public async Task ExibirUmaPágina(int index)
-        {
-            await conec.ApagarCorpo();
-            if (index == 0) await conec.AddPrimeiraPage(dadosPrimeiraPagina);
-            else await conec.AddOutraPage(dadosOutrasPaginas[index - 1]);
-        }
-
         internal static Geral Converter(Processo proc)
         {
             var dadosAdicionais = GetExtras(proc.NFe.Informações.infAdic);
@@ -163,8 +54,8 @@ namespace NFeFacil.DANFE.Processamento
             {
                 return new DadosAdicionais
                 {
-                    Dados = Analisar(extras?.infCpl),
-                    Fisco = Analisar(extras?.infAdFisco),
+                    Dados = extras?.infCpl,
+                    Fisco = extras?.infAdFisco,
                 };
             }
 
--- NFeFacil/View/PartesDANFE/CampoDadosAdicionais.xaml.cs
+++ NFeFacil/View/PartesDANFE/CampoDadosAdicionais.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Windows.UI.Xaml;
+﻿using NFeFacil.DANFE.Pacotes;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -9,9 +11,44 @@ namespace NFeFacil.View.PartesDANFE
     {
         GridLength AlturaCampo => DimensoesPadrao.CentimeterToLength(3.2);
 
+        public DadosAdicionais ContextoComplexo
+        {
+            set
+            {
+                LimparBloco();
+                if (value.Dados != null)
+                {
+                    var paragrafo = new Paragraph();
+                    paragrafo.Inlines.Add(new Run() { Text = "  DE INTERESSE DO CONTRIBUINTE:" });
+                    paragrafo.Inlines.Add(new LineBreak());
+                    paragrafo.Inlines.Add(new Run() { Text = value.Dados });
+                    bloco.Blocks.Add(paragrafo);
+                }
+                if (value.Fisco != null)
+                {
+                    var paragrafo = new Paragraph();
+                    paragrafo.Inlines.Add(new Run() { Text = "  DE INTERESSE DO FISCO:" });
+                    paragrafo.Inlines.Add(new LineBreak());
+                    paragrafo.Inlines.Add(new Run() { Text = value.Fisco });
+                    bloco.Blocks.Add(paragrafo);
+                }
+            }
+        }
+
         public CampoDadosAdicionais()
         {
             this.InitializeComponent();
+        }
+
+        private void LimparBloco()
+        {
+            if (bloco.Blocks.Count > 1)
+            {
+                for (int i = 1; i < bloco.Blocks.Count; i++)
+                {
+                    bloco.Blocks.RemoveAt(i);
+                }
+            }
         }
     }
 }

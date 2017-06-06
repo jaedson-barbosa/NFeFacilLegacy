220216807e78d974d4651fc7c955986097037854
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-06-05 23:17:37 -0300

    Melhorado o estilo do pivot personalizado; Corrigidos problemas na análise de vendas anual; Removido controle Guia, usado como titulo personalizado do pivot mas que não tem mais serventia.

--- NFeFacil/View/VendasAnuais.xaml.cs
+++ NFeFacil/View/VendasAnuais.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -7,12 +9,36 @@ namespace NFeFacil.View
     /// <summary>
     /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
     /// </summary>
-    public sealed partial class VendasAnuais : Page
+    public sealed partial class VendasAnuais : Page, IHambuguer
     {
         public VendasAnuais()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
             MainPage.Current.SeAtualizar(Symbol.Calendar, "Vendas anuais");
+        }
+
+        public ListView ConteudoMenu
+        {
+            get
+            {
+                var lista = new ListView()
+                {
+                    ItemsSource = new ObservableCollection<Controles.ItemHambuguer>
+                    {
+                        new Controles.ItemHambuguer(Symbol.Calendar, "Meses"),
+                        new Controles.ItemHambuguer(Symbol.People, "Clientes"),
+                    },
+                    SelectedIndex = 0
+                };
+                flipView.SelectionChanged += (sender, e) => lista.SelectedIndex = flipView.SelectedIndex;
+                lista.SelectionChanged += (sender, e) => flipView.SelectedIndex = lista.SelectedIndex;
+                return lista;
+            }
         }
     }
 }
--- NFeFacil/ViewModel/RelatorioVendasAnuais.cs
+++ NFeFacil/ViewModel/RelatorioVendasAnuais.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace NFeFacil.ViewModel
@@ -22,26 +21,14 @@ namespace NFeFacil.ViewModel
 
         public RelatorioVendasAnuais()
         {
-            DefinirTudo();
-
-            async void DefinirTudo()
-            {
-                await DefinirAnosDisponiveis();
-            }
-        }
-
-        private async Task DefinirAnosDisponiveis()
-        {
             using (var db = new NotasFiscais())
             {
-                AnosDisponiveis = await Task.Run(() => (from dado in db.Registro
-                                                        let ano = Convert.ToDateTime(dado.DataEmissao).Year
-                                                        orderby ano ascending
-                                                        select ano).Distinct().GerarObs());
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AnosDisponiveis)));
+                AnosDisponiveis = (from dado in db.Registro
+                                   let ano = Convert.ToDateTime(dado.DataEmissao).Year
+                                   orderby ano ascending
+                                   select ano).Distinct().GerarObs();
             }
         }
-
 
         private int anoEscolhido;
         public int AnoEscolhido
@@ -56,65 +43,64 @@ namespace NFeFacil.ViewModel
 
         private void AnoMudou()
         {
-            IEnumerable<NFe> notas;
             using (var db = new NotasFiscais())
             {
-                notas = from item in db.Registro
+                var notas = from item in db.Registro
                         let xml = XElement.Parse(item.XML)
                         let usarNFe = item.Status < 4
                         let nota = usarNFe ? xml.FromXElement<NFe>() : (xml.FromXElement<Processo>()).NFe
                         select nota;
-            }
 
-            var totalCliente = new List<TotalPorCliente>();
-            foreach (var item in notas)
-            {
-                var det = item.Informações;
-                if (totalCliente.Count(x => x.Doc == det.destinatário.Documento) == 0)
-                    totalCliente.Add(new TotalPorCliente { Doc = det.destinatário.Documento });
-                var tot = totalCliente.Single(x => x.Doc == det.destinatário.Documento);
-                tot.Mun = det.destinatário.endereco.NomeMunicipio;
-                tot.Nome = det.destinatário.nome;
-                foreach (var prod in det.produtos)
+                var totalCliente = new List<TotalPorCliente>();
+                foreach (var item in notas)
                 {
-                    tot.Quantidade += prod.Produto.QuantidadeComercializada;
+                    var det = item.Informações;
+                    if (totalCliente.Count(x => x.Doc == det.destinatário.Documento) == 0)
+                        totalCliente.Add(new TotalPorCliente { Doc = det.destinatário.Documento });
+                    var tot = totalCliente.Single(x => x.Doc == det.destinatário.Documento);
+                    tot.Mun = det.destinatário.endereco.NomeMunicipio;
+                    tot.Nome = det.destinatário.nome;
+                    foreach (var prod in det.produtos)
+                    {
+                        tot.Quantidade += prod.Produto.QuantidadeComercializada;
+                    }
+                    tot.Total += det.total.ICMSTot.vNF;
                 }
-                tot.Total += det.total.ICMSTot.vNF;
-            }
-            totalCliente.Add(new TotalPorCliente
-            {
-                Nome = "Total",
-                Quantidade = totalCliente.Sum(x => x.Quantidade),
-                Total = totalCliente.Sum(x => x.Total)
-            });
-            ResultadoCliente = (from item in totalCliente
-                                 orderby item.Total descending
-                                 select item).GerarObs();
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoCliente)));
+                totalCliente.Add(new TotalPorCliente
+                {
+                    Nome = "Total",
+                    Quantidade = totalCliente.Sum(x => x.Quantidade),
+                    Total = totalCliente.Sum(x => x.Total)
+                });
+                ResultadoCliente = (from item in totalCliente
+                                    orderby item.Total descending
+                                    select item).GerarObs();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoCliente)));
 
-            var totalMes = new List<TotalPorMes>(12);
-            for (int i = 1; i < 13; i++)
-            {
-                totalMes.Add(new TotalPorMes { Mês = i.ToString() });
-            }
-            foreach (var item in notas)
-            {
-                var det = item.Informações;
-                var data = Convert.ToDateTime(det.identificação.DataHoraEmissão);
-                foreach (var prod in det.produtos)
+                var totalMes = new List<TotalPorMes>(12);
+                for (int i = 1; i < 13; i++)
                 {
-                    totalCliente[data.Month - 1].Quantidade += prod.Produto.QuantidadeComercializada;
+                    totalMes.Add(new TotalPorMes { Mês = i.ToString() });
                 }
-                totalCliente[data.Month - 1].Total += det.total.ICMSTot.vNF;
+                foreach (var item in notas)
+                {
+                    var det = item.Informações;
+                    var data = Convert.ToDateTime(det.identificação.DataHoraEmissão);
+                    foreach (var prod in det.produtos)
+                    {
+                        totalMes[data.Month - 1].Quantidade += prod.Produto.QuantidadeComercializada;
+                    }
+                    totalMes[data.Month - 1].Total += det.total.ICMSTot.vNF;
+                }
+                totalMes.Add(new TotalPorMes
+                {
+                    Mês = "Total",
+                    Quantidade = totalMes.Sum(x => x.Quantidade),
+                    Total = totalMes.Sum(x => x.Total)
+                });
+                ResultadoMes = totalMes.GerarObs();
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoMes)));
             }
-            totalMes.Add(new TotalPorMes
-            {
-                Mês = "Total",
-                Quantidade = totalCliente.Sum(x => x.Quantidade),
-                Total = totalCliente.Sum(x => x.Total)
-            });
-            ResultadoMes = totalMes.GerarObs();
-            PropertyChanged(this, new PropertyChangedEventArgs(nameof(ResultadoMes)));
         }
 
         public sealed class TotalPorCliente

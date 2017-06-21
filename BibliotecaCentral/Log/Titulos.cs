e477e1f94700854be3f984106a042c4f5246aea1
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-06-21 15:53:33 -0300

    Removidos titulos desnecessários. Novamente diminuida a altura do item da lista.

--- BibliotecaCentral/Log/Titulos.cs
+++ BibliotecaCentral/Log/Titulos.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace BibliotecaCentral.Log
+﻿namespace BibliotecaCentral.Log
 {
     internal static class Titulos
     {
@@ -18,20 +16,12 @@ namespace BibliotecaCentral.Log
                     return "Erro catastrófico";
                 case TitulosComuns.Iniciando:
                     return "Iniciando";
-                case TitulosComuns.Lendo:
-                    return "Lendo";
-                case TitulosComuns.Processando:
-                    return "Processando";
-                case TitulosComuns.Finalizando:
-                    return "Finalizando";
                 case TitulosComuns.Log:
                     return nameof(Log);
                 case TitulosComuns.ValidaçãoConcluída:
                     return "Tudo certo";
-                case TitulosComuns.OperaçãoCancelada:
-                    return "Operação cancelada";
                 default:
-                    throw new Exception($"Existe um título comum que não foi analisado: {título}.");
+                    return null;
             }
         }
     }

708a253d3dd642392dc26f3321eaf1eeec81917f
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-06-27 15:12:57 -0300

    Criado controle de entrada numérica a partir do criado no É Primo, agora só resta implantá-lo.

---
+++ NFeFacil/View/Controles/EntradaNumerica.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Globalization;
+using System;
+using System.Text.RegularExpressions;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace NFeFacil.View.Controles
+{
+    public sealed partial class EntradaNumerica : UserControl
+    {
+        Regex regex = new Regex(@"[^\d,.]");
+        string original = string.Empty;
+        CultureInfo culturaPadrao = CultureInfo.InvariantCulture;
+
+        public string Format { get; set; }
+        public string Header { get; set; }
+        public double Number
+        {
+            get => (double)GetValue(NumberProperty);
+            set
+            {
+                var texto = value.ToString(Format, culturaPadrao);
+                txtNumber.Text = texto;
+                SetValue(NumberProperty, double.Parse(texto, culturaPadrao));
+            }
+        }
+
+        public static readonly DependencyProperty FormatProperty = DependencyProperty.Register("Format", typeof(string), typeof(EntradaNumerica), new PropertyMetadata(null));
+        public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(string), typeof(EntradaNumerica), new PropertyMetadata(null));
+        public static readonly DependencyProperty NumberProperty = DependencyProperty.Register("Number", typeof(double), typeof(EntradaNumerica), new PropertyMetadata(null));
+
+        public EntradaNumerica()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var input = (TextBox)sender;
+            if (input.FocusState != FocusState.Unfocused)
+            {
+                int pos = input.SelectionStart;
+                input.Text = regex.Replace(input.Text, string.Empty).Replace(',', '.');
+                input.SelectionStart = pos;
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var input = (TextBox)sender;
+            if (!string.IsNullOrEmpty(original))
+            {
+                input.Text = original;
+            }
+            else
+            {
+                input.Text = Number.ToString(culturaPadrao);
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var input = (TextBox)sender;
+            if (double.TryParse(input.Text, NumberStyles.AllowDecimalPoint, culturaPadrao, out double numero))
+            {
+                original = input.Text;
+                Number = numero;
+            }
+            else
+            {
+                input.Focus(FocusState.Keyboard);
+                //Agora deve ser exibido um aviso de que o formato está errado
+            }
+        }
+
+        private async void TextBox_Paste(object sender, TextControlPasteEventArgs e)
+        {
+            var input = (TextBox)sender;
+            var dataPackageView = Clipboard.GetContent();
+            if (dataPackageView.Contains(StandardDataFormats.Text))
+            {
+                var texto = await dataPackageView.GetTextAsync();
+                var regex = new Regex(@"[,.]");
+                var ocorrencias = regex.Matches(texto);
+                input.Text = regex.Replace(texto, string.Empty, ocorrencias.Count - 1);
+                e.Handled = true;
+            }
+        }
+    }
+}

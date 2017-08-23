c2eac4b55869e0a934fda259fbbfe55caf342420
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-08-23 11:44:43 -0300

    Criados conjuntos de dados.

--- NFeFacil/ViewRegistroVenda/DARV.xaml.cs
+++ NFeFacil/ViewRegistroVenda/DARV.xaml.cs
@@ -27,6 +27,32 @@ namespace NFeFacil.ViewRegistroVenda
 
     public struct ConjuntoDadosDARV
     {
-        
+        public DadosEmitente Emitente { get; set; }
+        public PacotesImpressaoGenericos.DadosCliente Cliente { get; set; }
+        public string DataVenda { get; set; }
+        public string IdVenda { get; set; }
+        public string Vendedor { get; set; }
+        public string CPFVendedor { get; set; }
+        public string Motorista { get; set; }
+        public DadosProduto[] Produtos { get; set; }
+        public string Desconto { get; set; }
+        public string Total { get; set; }
+        public string Observações { get; set; }
+        public string ChaveNFeRelacionada { get; set; }
+    }
+
+    public struct DadosEmitente
+    {
+        public string NomeFicticio { get; set; }
+        public PacotesImpressaoGenericos.Endereço Endereco { get; set; }
+        public string Email { get; set; }
+    }
+
+    public struct DadosProduto
+    {
+        public string Descricao { get; set; }
+        public string Quantidade { get; set; }
+        public string Valor { get; set; }
+        public string Total { get; set; }
     }
 }

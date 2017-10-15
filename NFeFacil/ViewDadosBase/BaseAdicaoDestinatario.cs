68ddd1079179c5485a1f91c102a850bff3e3fdc4
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-10-15 16:00:53 -0200

    Criada classe base de adição do destinatário com todos os métodos da antiga tela de adição do destinatário, que foi removida.

--- NFeFacil/ViewDadosBase/BaseAdicaoDestinatario.cs
+++ NFeFacil/ViewDadosBase/BaseAdicaoDestinatario.cs
@@ -6,55 +6,44 @@ using Windows.UI.Xaml.Navigation;
 using System;
 using NFeFacil.ItensBD;
 
-// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace NFeFacil.ViewDadosBase
 {
-    /// <summary>
-    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
-    /// </summary>
-    public sealed partial class AdicionarDestinatario : Page
+    public sealed class BaseAdicaoDestinatario
     {
-        private ClienteDI cliente;
-        private ILog Log = Popup.Current;
+        internal ClienteDI Cliente { get; private set; }
+        internal readonly ILog Log = Popup.Current;
 
-        public AdicionarDestinatario()
-        {
-            InitializeComponent();
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        internal void OnNavigatedTo(NavigationEventArgs e)
         {
             if (e.Parameter == null)
             {
-                cliente = new ClienteDI();
+                Cliente = new ClienteDI();
                 MainPage.Current.SeAtualizar(Symbol.Add, "Cliente");
             }
             else
             {
-                cliente = (ClienteDI)e.Parameter;
+                Cliente = (ClienteDI)e.Parameter;
                 MainPage.Current.SeAtualizar(Symbol.Edit, "Cliente");
             }
-            DataContext = new ClienteDataContext(ref cliente);
         }
 
-        private void Confirmar_Click(object sender, RoutedEventArgs e)
+        internal void Confirmar_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                if (new ValidadorDestinatario(cliente).Validar(Log))
+                if (new ValidadorDestinatario(Cliente).Validar(Log))
                 {
                     using (var db = new AplicativoContext())
                     {
-                        cliente.UltimaData = DateTime.Now;
-                        if (cliente.Id == Guid.Empty)
+                        Cliente.UltimaData = DateTime.Now;
+                        if (Cliente.Id == Guid.Empty)
                         {
-                            db.Add(cliente);
+                            db.Add(Cliente);
                             Log.Escrever(TitulosComuns.Sucesso, "Cliente salvo com sucesso.");
                         }
                         else
                         {
-                            db.Update(cliente);
+                            db.Update(Cliente);
                             Log.Escrever(TitulosComuns.Sucesso, "Cliente alterado com sucesso.");
                         }
                         db.SaveChanges();
@@ -68,7 +57,7 @@ namespace NFeFacil.ViewDadosBase
             }
         }
 
-        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        internal void Cancelar_Click(object sender, RoutedEventArgs e)
         {
             MainPage.Current.Retornar();
         }

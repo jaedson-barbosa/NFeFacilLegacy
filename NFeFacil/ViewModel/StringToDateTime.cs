ff4d02ce8ae4f7b68eac0974d0a0862aa91d2184
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-10-20 21:27:43 -0300

    Movidos conversores para pasta raiz da VM.

--- NFeFacil/ViewModel/StringToDateTime.cs
+++ NFeFacil/ViewModel/StringToDateTime.cs
@@ -2,7 +2,7 @@
 using System.Globalization;
 using Windows.UI.Xaml.Data;
 
-namespace NFeFacil.ViewModel.Converters
+namespace NFeFacil.ViewModel
 {
     public sealed class StringToDateTime : IValueConverter
     {

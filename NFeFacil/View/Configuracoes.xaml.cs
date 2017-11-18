89cb7a7f2e2aecdd4002c4e7e1bb826eec645d33
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-11-18 17:44:24 -0300

    Concluida configuração das compras dentro do aplicativo.

--- NFeFacil/View/Configuracoes.xaml.cs
+++ NFeFacil/View/Configuracoes.xaml.cs
@@ -12,6 +12,8 @@ namespace NFeFacil.View
     /// </summary>
     public sealed partial class Configuracoes : Page
     {
+        readonly ComprasInApp Compra = new ComprasInApp(ComprasInApp.Compras.Personalizacao);
+
         public Configuracoes()
         {
             this.InitializeComponent();
@@ -33,51 +35,63 @@ namespace NFeFacil.View
 
         async void UsarImagem(object sender, TappedRoutedEventArgs e)
         {
-            var brushAtual = MainPage.Current.ImagemBackground;
-            if (ConfiguracoesPermanentes.IDBackgroung == default(Guid))
+            if (await Compra.AnalisarCompra())
             {
-                ConfiguracoesPermanentes.IDBackgroung = Guid.NewGuid();
-            }
-            var caixa = new DefinirImagem(ConfiguracoesPermanentes.IDBackgroung, brushAtual);
-            if (await caixa.ShowAsync() == ContentDialogResult.Primary)
-            {
-                MainPage.Current.ImagemBackground = caixa.Imagem;
-                MainPage.Current.DefinirTipoBackground(TiposBackground.Imagem);
+                var brushAtual = MainPage.Current.ImagemBackground;
+                if (ConfiguracoesPermanentes.IDBackgroung == default(Guid))
+                {
+                    ConfiguracoesPermanentes.IDBackgroung = Guid.NewGuid();
+                }
+                var caixa = new DefinirImagem(ConfiguracoesPermanentes.IDBackgroung, brushAtual);
+                if (await caixa.ShowAsync() == ContentDialogResult.Primary)
+                {
+                    MainPage.Current.ImagemBackground = caixa.Imagem;
+                    MainPage.Current.DefinirTipoBackground(TiposBackground.Imagem);
+                }
             }
         }
 
-        private void UsarCor(object sender, TappedRoutedEventArgs e)
+        async void UsarCor(object sender, TappedRoutedEventArgs e)
         {
-            MainPage.Current.DefinirTipoBackground(TiposBackground.Cor);
+            if (await Compra.AnalisarCompra())
+            {
+                MainPage.Current.DefinirTipoBackground(TiposBackground.Cor);
+            }
         }
 
         async void EscolherTransparencia(object sender, TappedRoutedEventArgs e)
         {
-            var caixa = new EscolherTransparencia(ConfiguracoesPermanentes.OpacidadeBackground);
-            if (await caixa.ShowAsync() == ContentDialogResult.Primary)
+            if (await Compra.AnalisarCompra())
             {
-                ConfiguracoesPermanentes.OpacidadeBackground = caixa.Opacidade;
-                MainPage.Current.DefinirOpacidadeBackground(caixa.Opacidade);
+                var caixa = new EscolherTransparencia(ConfiguracoesPermanentes.OpacidadeBackground);
+                if (await caixa.ShowAsync() == ContentDialogResult.Primary)
+                {
+                    ConfiguracoesPermanentes.OpacidadeBackground = caixa.Opacidade;
+                    MainPage.Current.DefinirOpacidadeBackground(caixa.Opacidade);
+                }
             }
         }
 
-        private void Resetar(object sender, TappedRoutedEventArgs e)
+        async void Resetar(object sender, TappedRoutedEventArgs e)
         {
-            using (var db = new AplicativoContext())
+            if (await Compra.AnalisarCompra())
             {
-                if (ConfiguracoesPermanentes.IDBackgroung != default(Guid))
+                using (var db = new AplicativoContext())
                 {
-                    var img = db.Imagens.Find(ConfiguracoesPermanentes.IDBackgroung);
-                    if (img?.Bytes != null)
+                    if (ConfiguracoesPermanentes.IDBackgroung != default(Guid))
                     {
-                        img.Bytes = null;
-                        db.Update(img);
-                        db.SaveChanges();
+                        var img = db.Imagens.Find(ConfiguracoesPermanentes.IDBackgroung);
+                        if (img?.Bytes != null)
+                        {
+                            img.Bytes = null;
+                            db.Update(img);
+                            db.SaveChanges();
+                        }
                     }
                 }
+                ConfiguracoesPermanentes.OpacidadeBackground = 1;
+                MainPage.Current.DefinirTipoBackground(TiposBackground.Padrao);
             }
-            ConfiguracoesPermanentes.OpacidadeBackground = 1;
-            MainPage.Current.DefinirTipoBackground(TiposBackground.Padrao);
         }
     }
 }
--- NFeFacil/ViewNFe/NotasSalvas.xaml.cs
+++ NFeFacil/ViewNFe/NotasSalvas.xaml.cs
@@ -225,7 +225,7 @@ namespace NFeFacil.ViewNFe
             var nfe = processo.NFe;
             var analisador = new AnalisadorNFe(ref nfe);
             analisador.Desnormalizar();
-            if (await new ViewNFe.CriadorNFe(nfe).ShowAsync() == ContentDialogResult.Primary)
+            if (await new CriadorNFe(nfe).ShowAsync() == ContentDialogResult.Primary)
             {
                 Popup.Current.Escrever(TitulosComuns.Sucesso, "Nota de entrada criada. Agora verifique se todas as informações estão corretas.");
             }

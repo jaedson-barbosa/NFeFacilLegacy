fa1de0f198f0e80990bab9aeb8d25ca2f595275c
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2017-11-29 10:42:12 -0300

    1ª simplificação da visualização da NFe.

--- NFeFacil/ViewNFe/VisualizacaoNFe.xaml.cs
+++ NFeFacil/ViewNFe/VisualizacaoNFe.xaml.cs
@@ -8,7 +8,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Xml.Linq;
 using Windows.UI.Text;
 using Windows.UI.Xaml;
@@ -39,7 +38,7 @@ namespace NFeFacil.ViewNFe
             MainPage.Current.SeAtualizar(Symbol.View, "Visualizar NFe");
             ItemBanco = (NFeDI)e.Parameter;
             var xml = XElement.Parse(ItemBanco.XML);
-            List<PropriedadeHierárquica> propriedades;
+            IEnumerable<PropriedadeHierárquica> propriedades;
             if (ItemBanco.Status < (int)StatusNFe.Emitida)
             {
                 var nfe = xml.FromXElement<NFe>();
@@ -52,122 +51,73 @@ namespace NFeFacil.ViewNFe
                 ObjetoItemBanco = processo;
                 propriedades = ObterPropriedades(processo.NFe.Informacoes);
             }
-            var linear = PropriedadeHierarquicaToLinear(propriedades, 0);
-            linear.ForEach(x => AdicionarCampo(x.Texto, (EstilosTexto)x.Profundidade, x.Complementar));
+            ProcessarPropriedades(propriedades, 0);
             AtualizarBotoesComando();
         }
 
-        List<PropriedadeHierárquica> ObterPropriedades(object obj)
+        IEnumerable<PropriedadeHierárquica> ObterPropriedades(object obj)
         {
-            var retorno = new List<PropriedadeHierárquica>();
-            var tipo = obj.GetType();
-            foreach (var prop in tipo.GetProperties().Where(x => x.CanWrite
+            foreach (var prop in obj.GetType().GetProperties().Where(x => x.CanWrite
                 && x.GetCustomAttribute<System.Xml.Serialization.XmlIgnoreAttribute>() == null))
             {
                 var valor = prop.GetValue(obj);
                 if (valor != null)
                 {
-                    var tipoFilho = valor.GetType();
-                    if (tipoFilho.Namespace.Contains("NFeFacil") && !(valor is IEnumerable))
+                    var desc = prop.GetCustomAttribute<DescricaoPropriedade>();
+                    if (valor.GetType().Namespace.Contains("NFeFacil"))
                     {
-                        var desc = prop.GetCustomAttribute<DescricaoPropriedade>();
-                        retorno.Add(new PropriedadeHierárquica
+                        yield return new PropriedadeHierárquica
                         {
                             Nome = desc?.Descricao ?? prop.Name,
                             Valor = ObterPropriedades(valor)
-                        });
+                        };
                     }
-                    else if (valor is IEnumerable teste && !(valor is string))
+                    else if (valor is IEnumerable listaFilha && !(valor is string))
                     {
-                        var desc = prop.GetCustomAttribute<DescricaoPropriedade>();
-                        List<PropriedadeHierárquica> propriedadesFilhas = new List<PropriedadeHierárquica>();
-                        foreach (var item in teste)
+                        var tipoItem = listaFilha.GetType().GenericTypeArguments[0];
+                        var itemPersonalizado = tipoItem.Namespace.Contains("NFeFacil");
+                        foreach (var item in listaFilha)
                         {
-                            var tipoItem = item.GetType();
-                            object valorItem;
-                            if (tipoItem.Namespace.Contains("NFeFacil"))
+                            yield return new PropriedadeHierárquica
                             {
-                                valorItem = ObterPropriedades(item);
-                            }
-                            else
-                            {
-                                valorItem = item;
-                            }
-                            propriedadesFilhas.Add(new PropriedadeHierárquica
-                            {
-                                Nome = desc?.Descricao ?? prop.Name,
-                                Valor = valorItem
-                            });
+                                Nome = desc?.Descricao ?? tipoItem.Name,
+                                Valor = itemPersonalizado ? ObterPropriedades(item) : item
+                            };
                         }
-                        retorno.AddRange(propriedadesFilhas);
                     }
                     else
                     {
-                        var desc = prop.GetCustomAttribute<DescricaoPropriedade>();
                         var ext = prop.GetCustomAttribute<PropriedadeExtensivel>();
-                        if (desc != null)
+                        yield return new PropriedadeHierárquica
                         {
-                            retorno.Add(new PropriedadeHierárquica
-                            {
-                                Nome = desc.Descricao,
-                                Valor = valor
-                            });
-                        }
-                        else if (ext != null)
-                        {
-                            retorno.Add(new PropriedadeHierárquica
-                            {
-                                Nome = ext.NomeExtensão,
-                                Valor = ext.ObterValor(valor)
-                            });
-                        }
-                        else
-                        {
-                            retorno.Add(new PropriedadeHierárquica
-                            {
-                                Nome = prop.Name,
-                                Valor = valor
-                            });
-                        }
+                            Nome = ext?.NomeExtensão ?? desc?.Descricao ?? prop.Name,
+                            Valor = ext?.ObterValor(valor) ?? valor
+                        };
                     }
                 }
             }
-            return retorno;
         }
 
-        List<PropriedadeLinear> PropriedadeHierarquicaToLinear(List<PropriedadeHierárquica> hierarquia, int profundidade)
+        void ProcessarPropriedades(IEnumerable<PropriedadeHierárquica> hierarquia, int profundidade)
         {
-            var retorno = new List<PropriedadeLinear>();
-            for (int i = 0; i < hierarquia.Count; i++)
+            foreach (var atual in hierarquia)
             {
-                var atual = hierarquia[i];
-                if (atual.Valor is List<PropriedadeHierárquica> subhierarquia)
+                if (atual.Valor is IEnumerable<PropriedadeHierárquica> subhierarquia)
                 {
-                    retorno.Add(new PropriedadeLinear
-                    {
-                        Profundidade = profundidade,
-                        Texto = atual.Nome
-                    });
-                    retorno.AddRange(PropriedadeHierarquicaToLinear(subhierarquia, profundidade + 1));
+                    AdicionarCampo(atual.Nome, (EstilosTexto)profundidade);
+                    ProcessarPropriedades(subhierarquia, profundidade + 1);
                 }
                 else
                 {
-                    retorno.Add(new PropriedadeLinear
-                    {
-                        Profundidade = (int)EstilosTexto.BodyTextBlockStyle,
-                        Texto = atual.Nome,
-                        Complementar = atual.Valor.ToString()
-                    });
+                    AdicionarCampo(atual.Nome, EstilosTexto.BodyTextBlockStyle, atual.Valor.ToString());
                 }
             }
-            return retorno;
         }
 
-        void AdicionarCampo(string texto, EstilosTexto estilo, string textoComplementar)
+        void AdicionarCampo(string texto, EstilosTexto estilo, string textoComplementar = null)
         {
-            texto = ProcessarTitulo(texto);
             var linha = new Run() { Text = texto };
-            AplicarEstilo(linha);
+            AplicarEstilo();
 
             if (textoComplementar != null)
             {
@@ -180,65 +130,40 @@ namespace NFeFacil.ViewNFe
             if (textoComplementar != null)
             {
                 linha = new Run() { Text = textoComplementar };
-                AplicarEstilo(linha);
+                AplicarEstilo();
                 visualizacao.Inlines.Add(linha);
             }
 
             visualizacao.Inlines.Add(new LineBreak());
 
-            void AplicarEstilo(Run linhaUsada)
+            void AplicarEstilo()
             {
                 switch (estilo)
                 {
                     case EstilosTexto.HeaderTextBlockStyle:
-                        linhaUsada.FontWeight = FontWeights.Light;
-                        linhaUsada.FontSize = 46;
+                        linha.FontWeight = FontWeights.Light;
+                        linha.FontSize = 46;
                         break;
                     case EstilosTexto.SubheaderTextBlockStyle:
-                        linhaUsada.FontWeight = FontWeights.Light;
-                        linhaUsada.FontSize = 34;
+                        linha.FontWeight = FontWeights.Light;
+                        linha.FontSize = 34;
                         break;
                     case EstilosTexto.TitleTextBlockStyle:
-                        linhaUsada.FontWeight = FontWeights.SemiLight;
-                        linhaUsada.FontSize = 24;
+                        linha.FontWeight = FontWeights.SemiLight;
+                        linha.FontSize = 24;
                         break;
                     case EstilosTexto.SubtitleTextBlockStyle:
-                        linhaUsada.FontWeight = FontWeights.Normal;
-                        linhaUsada.FontSize = 20;
+                        linha.FontWeight = FontWeights.Normal;
+                        linha.FontSize = 20;
                         break;
                 }
             }
-        }
-
-        string ProcessarTitulo(string titulo)
-        {
-            StringBuilder construtor = new StringBuilder();
-            construtor.Append(char.ToUpper(titulo[0]));
-            for (int i = 1; i < titulo.Length; i++)
-            {
-                if (char.IsUpper(titulo[i]) && char.IsLower(titulo[i - 1]))
-                {
-                    construtor.Append($" {titulo[i]}");
-                }
-                else
-                {
-                    construtor.Append(titulo[i]);
-                }
-            }
-            return construtor.ToString();
         }
 
         struct PropriedadeHierárquica
         {
             public string Nome { get; set; }
             public object Valor { get; set; }
-        }
-
-        struct PropriedadeLinear
-        {
-            public string Texto { get; set; }
-            public string Complementar { get; set; }
-            public int Profundidade { get; set; }
         }
 
         enum EstilosTexto

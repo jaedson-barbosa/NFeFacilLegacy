345feda144d2fbb09c02d11c85f664d91cdf3695
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2018-03-24 14:19:50 -0300

    Criados mais alguns projetos e movidas classes pros devidos projetos, falta apenas concertar detalhes de dependência (num primeiro momento).

---
+++ Produto/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Produto
+{
+    public class Class1
+    {
+    }
+}

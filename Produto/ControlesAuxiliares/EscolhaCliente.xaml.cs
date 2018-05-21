c1e5685258ae50a0aaa578f208fab58854f7649e
Jaedson Barbosa Serafim <jaedson_9@hotmail.com>
2018-05-21 13:56:58 -0300

    Criado controle para escolha dos clientes, ele será usado na NFe, NFCe e registro não-fiscal.

---
+++ Produto/ControlesAuxiliares/EscolhaCliente.xaml.cs
@@ -0,0 +1,48 @@
+﻿using BaseGeral.Buscador;
+using BaseGeral.ItensBD;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// O modelo de item de Controle de Usuário está documentado em https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Venda.ControlesAuxiliares
+{
+    public sealed partial class EscolhaCliente : UserControl
+    {
+        public static DependencyProperty ClienteSelecionadoProperty = DependencyProperty.Register(
+            nameof(ClienteSelecionado),
+            typeof(ClienteDI),
+            typeof(EscolhaCliente),
+            new PropertyMetadata(null, (x, y) => ((EscolhaCliente)x).ClienteSelecionado = (ClienteDI)y.NewValue));
+
+        public ClienteDI ClienteSelecionado
+        {
+            get => (ClienteDI)GetValue(ClienteSelecionadoProperty);
+            set => SetValue(ClienteSelecionadoProperty, value);
+        }
+
+        BuscadorCliente Clientes { get; }
+
+        public EscolhaCliente()
+        {
+            InitializeComponent();
+            Clientes = new BuscadorCliente();
+        }
+
+        private void BuscarCliente(object sender, TextChangedEventArgs e)
+        {
+            var busca = ((TextBox)sender).Text;
+            Clientes.Buscar(busca);
+        }
+
+        private void GridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            var input = (GridView)sender;
+            var cliente = input.SelectedItem;
+            if (cliente != null)
+            {
+                input.ScrollIntoView(cliente, ScrollIntoViewAlignment.Leading);
+            }
+        }
+    }
+}
